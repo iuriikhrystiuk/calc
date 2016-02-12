00eb7b1d868d19a735ffd9710a3010cf3fd187fa
Iurii <yura.1st.psy@gmail.com>
2016-02-12 19:10:12 +0200

    Glob version change.

--- deploy.cmd
+++ deploy.cmd
@@ -134,7 +134,7 @@ echo Running grunt build
 if EXIST "%DEPLOYMENT_SOURCE%\Gruntfile.js" (
     :: Hotfix for grunt and npm dependencies http://stackoverflow.com/a/35153955
     pushd ./node_modules/grunt
-    call !NPM_CMD! install glob@^6.0.4 --save
+    call !NPM_CMD! install glob@6.0.4 --save
     popd
     
     pushd %DEPLOYMENT_SOURCE%
