a52eda24faf0d5a15688c5de0bc88063a324345c
Iurii <yura.1st.psy@gmail.com>
2016-02-12 14:36:47 +0200

    Initial application commit
    Initial application commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-copy');
+    grunt.loadNpmTasks('grunt-include-source');
+    grunt.loadNpmTasks('grunt-serve');
+    
+    // Project configuration.
+    grunt.initConfig({
+        app: {
+            scripts: ['scripts/**/*.js']
+        },
+        wiredep: {
+            app: {
+                src: 'src/index.html'
+            }
+        },
+        includeSource: {
+            options: {
+                basePath: 'src/',
+                baseUrl: '',
+                ordering: 'top-down',
+                templates: {
+                    html: {
+                        js: '<script src="{filePath}"></script>',
+                        css: '<link rel="stylesheet" type="text/css" href="{filePath}" />'
+                    }
+                }
+            },
+            app: {
+                files: {
+                    'src/index.html': 'src/index.html',
+                }
+            }
+        },
+        useminPrepare: {
+            html: 'src/index.html',
+            options: {
+                dest: 'dist',
+                flow: {
+                    html: {
+                        steps: {
+                            js: ['concat', 'uglifyjs'],
+                            css: ['concat', 'cssmin']
+                        },
+                        post: {}
+                    }
+                }
+            }
+        },
+        copy: {
+            html: {
+                src: 'src/index.html',
+                dest: 'dist/index.html'
+            }
+        },
+        usemin: {
+            html: ['dist/index.html']
+        },
+        serve: {
+            options: {
+                port: 9000,
+            }
+        }
+    });
+
+
+    // Default task(s).
+    grunt.registerTask('build', [
+        'wiredep',
+        'includeSource',
+        'copy:html',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'usemin']);
+
+    grunt.registerTask('default', ['build', 'serve'])
+};
