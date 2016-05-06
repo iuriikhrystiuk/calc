99c8a977e311ff0f6e9f432defb897a42c3486ad
Iurii Khrystiuk <ikhryst@softserveinc.com>
2016-05-06 17:14:17 +0300

    Added base for building formulas.

---
+++ src/scripts/controllers/BuildCtrl.js
@@ -0,0 +1,72 @@
+(function () {
+    function BuildController($scope, lexer, evaluator) {
+
+        $scope.formula = null;
+        $scope.result = '';
+        $scope.errors = null;
+        $scope.missingIdentifiers = null;
+
+        function _populateContext(context) {
+            var populatedItems = 0;
+            if ($scope.missingIdentifiers) {
+                angular.forEach(context, function (item) {
+                    angular.forEach($scope.missingIdentifiers, function (ident) {
+                        if (item.identifier.value === ident.identifier.value) {
+                            item.value = ident.value;
+                            populatedItems++;
+                            return false;
+                        }
+
+                        return true;
+                    });
+                });
+            }
+
+            return populatedItems === context.length;
+        }
+
+        function _parse() {
+            try {
+                $scope.errors = null;
+                $scope.result = '';
+                var tokens = lexer.parse($scope.formula);
+                var context = [];
+                angular.forEach(tokens, function (item) {
+                    if (item.type === 'IDENTIFIER') {
+                        var alreadyExists = false;
+                        angular.forEach(this, function (ident) {
+                            if (item.value === ident.identifier.value) {
+                                alreadyExists = true;
+                                return false;
+                            }
+
+                            return true;
+                        });
+
+                        if (!alreadyExists) {
+                            this.push({
+                                identifier: item,
+                                value: null
+                            });
+                        }
+                    }
+                }, context);
+                if (!_populateContext(context)) {
+                    $scope.missingIdentifiers = context;
+                }
+                else {
+                    $scope.result = evaluator.evaluate(tokens, context);
+                }
+            } catch (error) {
+                $scope.errors = error.message || error;
+            }
+
+        }
+
+        $scope.parse = _parse;
+    }
+
+    BuildController.$inject = ['$scope', 'lexer', 'evaluator'];
+
+    angular.module('dps').controller('BuildCtrl', BuildController);
+} ());
