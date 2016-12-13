ea02e61a158d1277bca7d1130a0bffa6dc333188
Iurii Khrystiuk <ikhryst@softserveinc.com>
2016-12-13 13:11:33 +0200

    Fixing js hints.

--- src/scripts/controllers/PolygonCtrl.js
+++ src/scripts/controllers/PolygonCtrl.js
@@ -36,6 +36,67 @@
             return false;
         }
 
+        function hasAdjacents(dot, possibleDot) {
+            return possibleDot.adjacent.some(function (item) {
+                return item.x === dot.x && item.y === dot.y;
+            });
+        }
+
+        function rotateCoordinates(angle, dot, coordinates, skipIndices) {
+            return coordinates.map(function (item, idx) {
+                if (skipIndices.indexOf(idx) >= 0) {
+                    return null;
+                }
+
+                // translate coordinates through moving and rotating it.
+                return Math.cos(angle) * (item.y - dot.y) - Math.sin(angle) * (item.x - dot.x);
+            });
+        }
+
+        function isNormalSide(coordinates) {
+            var allPositive = coordinates.reduce(function (memo, item) {
+                return memo && (item === null || item >= 0);
+            }, true);
+            var allNegative = coordinates.reduce(function (memo, item) {
+                return memo && (item === null || item <= 0);
+            }, true);
+
+            return allPositive || allNegative;
+        }
+
+        function getNearestLine(dot, lines) {
+            return lines.reduce(function (memo, item, idx) {
+                var distanceFrom = calculateDistance(dot, item.from);
+                var distanceTo = calculateDistance(dot, item.to);
+                if (memo.distanceFrom === null) {
+                    memo.distanceFrom = distanceFrom;
+                    memo.distanceTo = distanceTo;
+                    memo.line = item;
+                } else if ((distanceFrom + distanceTo) < (memo.distanceFrom + memo.distanceTo)) {
+                    var intercects = lines.reduce(function (intercetion, line, index) {
+                        if (idx === index) {
+                            return intercetion;
+                        }
+
+                        return intercetion || checkIntercection(line, {
+                            from: dot,
+                            to: item.from
+                        }) || checkIntercection(line, {
+                            from: dot,
+                            to: item.to
+                        });
+                    }, false);
+                    if (!intercects) {
+                        memo.distanceFrom = distanceFrom;
+                        memo.distanceTo = distanceTo;
+                        memo.line = item;
+                    }
+                }
+
+                return memo;
+            }, { distanceFrom: null, distanceTo: null, line: null });
+        }
+
         $scope.paint = function () {
             var copy = angular.copy($scope.dots);
             copy = copy.map(function (item) {
@@ -51,26 +112,12 @@
                         continue;
                     }
                     var possibleDot = copy[i];
-                    if (possibleDot.adjacent.some(function (item) {
-                        return item.x === currentDot.x && item.y === currentDot.y;
-                    })) {
+                    if (hasAdjacents(currentDot, possibleDot)) {
                         continue;
                     }
                     var currentAngle = getAngle(currentDot, possibleDot);
-                    var allYs = copy.map(function (item, idx) {
-                        if (idx === i || idx === index) {
-                            return null;
-                        }
-                        // translate coordinates through moving and rotating it.
-                        return Math.cos(currentAngle) * (item.y - currentDot.y) - Math.sin(currentAngle) * (item.x - currentDot.x);
-                    });
-                    var allPositive = allYs.reduce(function (memo, item) {
-                        return memo && (item === null || item >= 0);
-                    }, true);
-                    var allNegative = allYs.reduce(function (memo, item) {
-                        return memo && (item === null || item <= 0);
-                    }, true);
-                    if (allPositive || allNegative) {
+                    var allYs = rotateCoordinates(currentAngle, currentDot, copy, [i, index]);
+                    if (isNormalSide(allYs)) {
                         currentDot.adjacent.push(possibleDot);
                         possibleDot.adjacent.push(currentDot);
                         lines.push({
@@ -85,46 +132,17 @@
             var itemsInside = copy.filter(function (item) {
                 return item.adjacent.length === 0;
             });
-            for (var i = 0; i < itemsInside.length; i++) {
-                var nearest = lines.reduce(function (memo, item, idx) {
-                    var distanceFrom = calculateDistance(itemsInside[i], item.from);
-                    var distanceTo = calculateDistance(itemsInside[i], item.to);
-                    if (memo.distanceFrom === null) {
-                        memo.distanceFrom = distanceFrom;
-                        memo.distanceTo = distanceTo;
-                        memo.line = item;
-                    } else if ((distanceFrom + distanceTo) < (memo.distanceFrom + memo.distanceTo)) {
-                        var intercects = lines.reduce(function (intercetion, line, index) {
-                            if (idx === index) {
-                                return intercetion;
-                            }
-
-                            return intercetion || checkIntercection(line, {
-                                from: itemsInside[i],
-                                to: item.from
-                            }) || checkIntercection(line, {
-                                from: itemsInside[i],
-                                to: item.to
-                            });
-                        }, false);
-                        if (!intercects) {
-                            memo.distanceFrom = distanceFrom;
-                            memo.distanceTo = distanceTo;
-                            memo.line = item;
-                        }
-                    }
-
-                    return memo;
-                }, { distanceFrom: null, distanceTo: null, line: null });
+            for (var idx = 0; idx < itemsInside.length; idx++) {
+                var nearest = getNearestLine(itemsInside[idx], lines);
                 if (nearest.line) {
-                    var index = lines.indexOf(nearest.line);
+                    index = lines.indexOf(nearest.line);
                     lines.splice(index, 1);
                     lines.push({
-                        from: itemsInside[i],
+                        from: itemsInside[idx],
                         to: nearest.line.from
                     });
                     lines.push({
-                        from: itemsInside[i],
+                        from: itemsInside[idx],
                         to: nearest.line.to
                     });
                 }
